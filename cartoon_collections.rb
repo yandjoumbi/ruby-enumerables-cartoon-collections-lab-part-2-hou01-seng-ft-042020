230fd022f6d80ea9c07174aa85735706044d26f2
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-17 14:20:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,7 +39,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.each do |element|
     for element in find_valid_calls
-      if element == "Earth!" || element == "Wind!" || element == "Fire!" || element== "Water!" || element== "Heart!"
+      #if element == "Earth!" || element == "Wind!" || element == "Fire!" || element== "Water!" || element== "Heart!"
         return element
     end
   end
