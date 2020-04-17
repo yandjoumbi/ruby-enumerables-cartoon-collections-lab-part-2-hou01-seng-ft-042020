9856db9a134e27573a255c2c27f9b2a0bb7ca5a4
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-17 14:56:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -42,7 +42,9 @@ def find_valid_calls(planeteer_calls)
       if element == "Earth!" || element == "Wind!" || element == "Fire!" || element== "Water!" || element== "Heart!"
         return element
     end
-
+       if element ==0
+         return nill
+       end
 end
   planeteer_calls
 end
